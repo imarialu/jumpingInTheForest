8a5fe1fae19650cb2a253cd6fab3b10c78cf3e68
imarialu <marialuizaprado13@gmail.com>
2025-08-28 10:31:23 -0300

    cor e rolagem de seleção do menu adicionadas

--- code/Menu.py
+++ code/Menu.py
@@ -1,5 +1,5 @@
 import pygame.image
-from code.Const import WIN_WIDTH, COLOR_MENU_TITLE, MENU_OPTION, COLOR_MENU_OPTION
+from code.Const import WIN_WIDTH, COLOR_MENU_TITLE, MENU_OPTION, COLOR_MENU_OPTION, COLOR_SELECTED_OPTION
 from pygame.font import Font
 from pygame import Surface, Rect
 
@@ -10,12 +10,13 @@ class Menu:
         self.rect = self.surf.get_rect()
 
     def run(self):
+        menu_option = 0
         # Adiciona som ao menu
         pygame.mixer_music.load('./asset/menu_sound.wav')
         pygame.mixer_music.play(-1)
 
         while True:
-            # Atualiza a imagem
+            # Desenha a imagem
             self.window.blit(source=self.surf, dest=self.rect)  # "Desenha" a imagem no retângulo
 
             # Adicionando e estlizando o texto do menu
@@ -25,8 +26,11 @@ class Menu:
 
             # Adicionando e estlizando as opções do menu
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, MENU_OPTION[i], COLOR_MENU_OPTION, ((WIN_WIDTH / 2), 220 + 30 * i))
-
+                # Adiciona outra cor a opção selecionada
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_SELECTED_OPTION, ((WIN_WIDTH / 2), 220 + 30 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_MENU_OPTION, ((WIN_WIDTH / 2), 220 + 30 * i))
             # Atualiza a tela
             pygame.display.flip()
 
@@ -35,7 +39,22 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Fecha a janela
                     quit()  # Finaliza o pygame
-
+                # Adiciona o evento de seleção
+                if event.type == pygame.KEYDOWN:
+                    # Seleção com a rolagem para baixo
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    # Seleção com a rolagem para cima
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN:
+                        return MENU_OPTION[menu_option]
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
