ef7e01ef375e10359de54174aec1324a4ec30350
imarialu <marialuizaprado13@gmail.com>
2025-08-31 17:33:36 -0300

    função de pular e andar do jogador criadas

---
+++ code/Player.py
@@ -0,0 +1,36 @@
+import pygame.key
+
+from code.Entity import Entity
+
+
+class Player(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+        self.jump = False
+        self.speed = 12
+        self.gravity = 1
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            if self.rect.y == 190 and self.jump == False:
+                self.jump = True
+
+        if keys[pygame.K_LEFT]:
+            if not self.rect.x <= 0:
+                self.rect.x -= 3
+
+        print(self.rect.x)
+        if keys[pygame.K_RIGHT]:
+            if not self.rect.x >= 560:
+                self.rect.x += 3
+
+        if self.jump == True:
+            self.rect.y -= self.speed
+            self.speed -= self.gravity
+
+        if self.rect.y == 190 and self.jump == True:
+            self.jump = False
+            self.speed = 12
+
+        pass
